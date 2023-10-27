d38d3e159f6be898775af9985156708d25e76315
Martín Castro <mcastro_20@alu.uabcs.mx>
2023-10-27 14:56:53 -0700

    feat: consumir API

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ActivityIndicator } from 'react-native';
+import { fetchData } from './fetchData';
+import { Suspense, startTransition } from 'react';
 
+
+const apiData = fetchData("https://pokeapi.co/api/v2/generation/");
+const data = apiData.read();
+console.log(data.name);
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  startTransition(() => {
+    
+    return (
+      <View >
+        <Suspense fallback={<ActivityIndicator />}>
+          <Text>{data.results}</Text>
+        </Suspense>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      <StatusBar style="auto" />
+  </View>
+);
 });
+}
