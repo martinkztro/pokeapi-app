5354760d858c1651d7c53c8dee17b082bd4ccf76
JazzielGod <jazzielbh@gmail.com>
2023-10-27 14:59:38 -0700

    Update

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { fetchData } from './hooks/fetchData';
+import { Suspense, startTransition } from 'react';
 
+const apiData = fetchData("https://pokeapi.co/api/v2/generation/");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       
+        <Suspense fallback={<Text>Loading...</Text>}>
+          <Text> {apiData.results[0].name}  </Text>
+        </Suspense>
+       
       <StatusBar style="auto" />
     </View>
   );
