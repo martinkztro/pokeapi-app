31542f69191881f3e81ae3deccdda3d64b0ef902
JazzielGod <jazzielbh@gmail.com>
2023-10-30 13:44:34 -0700

    update - errores

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, ActivityIndicator } from 'react-native';
-import { fetchData } from './fetchData';
-import { Suspense, startTransition } from 'react';
-
+import { StatusBar } from "expo-status-bar";
+import { Text, View, ActivityIndicator } from "react-native";
+import { fetchData } from "./fetchData";
+import { Suspense, startTransition } from "react";
 
 const apiData = fetchData("https://pokeapi.co/api/v2/generation/");
 const data = apiData.read();
 console.log(data.name);
 export default function App() {
   startTransition(() => {
-    
     return (
-      <View >
+      <View>
         <Suspense fallback={<ActivityIndicator />}>
           <Text>{data.results}</Text>
         </Suspense>
 
-      <StatusBar style="auto" />
-  </View>
-);
-});
+        <StatusBar style="auto" />
+      </View>
+    );
+  });
 }
